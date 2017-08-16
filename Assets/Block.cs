d5454882ff9578afdeca05ffcc32d8bacfcd9e26
Ravi <accounts1237@naver.com>
2017-08-16 21:09:58 +0900

    no message

---
+++ Assets/Block.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Block : MonoBehaviour {
+
+    // Block Number
+    public int number;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Create(int number)
+    {
+        this.number = number;
+        GetComponent<SpriteRenderer>().color = new Color(40 * ((number / 2) - 1), 0, 0);
+    }
+}
---
+++ Assets/TouchController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour {
+
+    public 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("ee");
+
+        if (Input.touchCount > 0)
+        {
+            Debug.Log(Input.touchCount);
+        }
+	}
+
+    private void OnMouseDown()
+    {
+        
+    }
+}
